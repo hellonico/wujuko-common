d13064b5124b86e402c6b8869eb86aca0fe0f258
Damion Junk <junkda@gmail.com>
2012-10-18 23:00:11 -0400

    initial import

---
+++ project.clj
@@ -0,0 +1,12 @@
+(defproject wujuko-common "0.1.0-SNAPSHOT"
+  :description "A not so organized collection of utility functions."
+  :url "http://damionjunk.com"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.4.0"]
+                 [org.clojure/tools.cli "0.2.2"]
+                 [org.clojure/tools.logging "0.2.3"]
+                 [org.slf4j/slf4j-log4j12 "1.6.4"]
+                 [org.apache.commons/commons-compress "1.4.1"]
+                 [cheshire "4.0.2"]
+                 [clj-time "0.4.2"]])
---
+++ src/wujuko_common/zio.clj
@@ -0,0 +1,62 @@
+(ns wujuko-common.zio
+  (:require [clojure.java.io :as io])
+  (:import [java.io FileOutputStream PrintWriter]
+           [org.apache.commons.compress.compressors.gzip
+            GzipCompressorInputStream GzipCompressorOutputStream]
+           [org.apache.commons.compress.compressors.bzip2
+            BZip2CompressorInputStream BZip2CompressorOutputStream]))
+
+;;
+;; Use zreader and zwriter to obtain a reader or writer that is
+;; compressed if the provided filename ends in .bz2 or .gz
+;;
+
+(defn- reader
+  "Fallback default reader if the stream isn't compressed."
+  [filename]
+  (-> filename io/file io/input-stream io/reader))
+
+;;
+;; Pass 'true' to GzipCompressorInputStream to tell it to handle
+;; multipart gzip files.
+(defn- gzip-reader
+  "Correctly handles multi-member gzip files, default java gzip does not."
+  [filename]
+  (-> filename io/file io/input-stream (GzipCompressorInputStream. true) io/reader))
+
+(defn- bzip2-reader
+  "Reader for bzip2 files."
+  [filename]
+  (-> filename io/file io/input-stream BZip2CompressorInputStream. io/reader))
+
+(defn- writer
+  "Default writer if compressed output is not desired."
+  [filename]
+  (-> filename io/file io/output-stream io/writer))
+
+(defn- gzip-writer
+  "Writer for gzip files."
+  [filename]
+  (-> filename io/file io/output-stream GzipCompressorOutputStream. io/writer))
+
+(defn- bzip2-writer
+  "Writer for bzip2 files."
+  [filename]
+  (-> filename io/file io/output-stream BZip2CompressorOutputStream. io/writer))
+
+(defn zreader
+  "Naive reader dispatch using file extension to determine file type."
+  [filename]
+  (let [fn (if (= java.net.URL (type filename)) (.getFile filename) filename)]
+    (cond
+      (.endsWith fn ".gz") (gzip-reader filename)
+      (.endsWith fn ".bz2") (bzip2-reader filename)
+      :else (reader filename))))
+
+(defn zwriter
+  "Naive writer dispatch using file extension to determine file type."
+  [filename]
+  (cond
+    (.endsWith filename ".gz") (gzip-writer filename)
+    (.endsWith filename ".bz2") (bzip2-writer filename)
+    :else (writer filename)))
