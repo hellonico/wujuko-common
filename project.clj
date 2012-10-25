8535d758d40a1188367ff9bc3c4eb52ee0fdbbe4
Damion Junk <junkda@gmail.com>
2012-10-25 16:15:45 -0400

    develop version bump to snapshot

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject wujuko-common "0.1.1"
+(defproject wujuko-common "0.1.2-SNAPSHOT"
   :description "A not so organized collection of utility functions."
   :url "http://damionjunk.com"
   :license {:name "Eclipse Public License"
