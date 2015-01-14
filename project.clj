2ecee94fabe13eb1d6b183319c95402f515348c0
hellonico <hellonico@gmail.com>
2015-01-14 13:10:47 +0900

    for quick release

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject wujuko-common "0.1.3"
+(defproject hellonico/wujuko-common "0.1.3"
   :description "A not so organized collection of utility functions."
   :url "http://damionjunk.com"
   :license {:name "Eclipse Public License"
